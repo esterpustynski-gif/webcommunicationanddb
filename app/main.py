bfff91acc13d0454e760fd848b3c25de01e6a190
Anna-Ester <ester.pustynski@gmail.com>
2026-04-02 11:46:44 +0300

    -

--- app/main.py
+++ app/main.py
@@ -1,32 +1,41 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+from app.db import get_conn
+
 
 app = FastAPI()
 
-@app.get ("/rooms")
-def get_rooms():
-    rooms = [
-        {"room number": 100,
-        "room type": "single",
-        "price": 75},
-        {"room number": 101,
-        "room type": "double",
-        "price": 100},
-        {"room number": 102,
-        "room type": "suite",
-        "price": 150}
-]
+origins = ["*"] # Change to the real front end origin in production
 
-@app.get("/api/rooms")
-def get_rooms():
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Main route for this API
+@app.get("/")
+def read_root(): 
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("SELECT 'hello postgres' ")
+        result = cur.fetchone()
+    # f-string concatenation
+    return { "msg": f"Hotel API!", "db_status": result }
+
+# List all rooms 
+@app.get("/rooms")
+def get_rooms(): 
+    rooms = [
+        { "room_number": 101, "room_type": "single room", "price": 80 },
+        { "room_number": 202, "room_type": "double room", "price": 120 },
+        { "room_number": 404, "room_type": "suite", "price": 500 }
+    ]
     return rooms
 
-@app.get("/")
-def read_root():
-    return {"msg": "Hi, yelloooo!"}
-@app.get("/api/ip")
-def get_ip(request: Request):
-    return {"ip": request.client.host}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"id": item_id, "q": q}
+# Create booking
+@app.post("/bookings")
+def create_booking():
+    return { "msg": "Booking created!"}
